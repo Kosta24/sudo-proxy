1d820b8a815caf0433b17e836ba23de2d2f9c3d3
Kosta24 <109385124+Kosta24@users.noreply.github.com>
2024-08-02 23:46:41 +0200

    Update headers.ts

--- src/utils/headers.ts
+++ src/utils/headers.ts
@@ -47,10 +47,7 @@ export function getProxyHeaders(headers: Headers): Headers {
     copyHeader(headers, output, entry[0], entry[1]);
   });
 
-   // Imposta l'header Origin se non esiste già
-  if (!headers.has('Origin')) {
-    output.set('Origin', 'https://your-custom-origin.com');
-  }
+  output.set('Origin', 'https://www.vidbinge.com);
 
   return output;
 }
